21e6f104a7b701b445c434b2bd4aa86f950e5d13
Goose <a.bulman6453@gmail.com>
2023-10-25 00:47:45 +0100

    Initial commit
    First push of main files

---
+++ effects.py
@@ -0,0 +1,42 @@
+import pygame, time
+
+def Blur(image, radius):
+    currentTime = time.time()
+    width = image.get_width()
+    height = image.get_height()
+    newSurf = pygame.surface.Surface((width, height))
+    for x in range(width):
+        for y in range(height):
+            pixels = FetchPixelsAsCircle(image, x, y, radius)
+            fR, fG, fB = 0, 0, 0
+            area = 0
+            for column in pixels:
+                for row in column:
+                    area += 1
+                    if row[2] == 'y':
+                        fR += image.get_at((row[0], row[1])).r
+                        fG += image.get_at((row[0], row[1])).g
+                        fB += image.get_at((row[0], row[1])).b
+            fR = fR / area
+            fG = fG / area
+            fB = fB / area
+            newSurf.set_at((x,y), (fR, fG, fB))
+    print("Done in " + str(time.time() - currentTime) + " seconds.")
+    return newSurf
+
+def FetchPixelsAsCircle(image, x, y, radius):
+    radiusSquared = radius * radius
+    imageWidth = image.get_width()
+    imageHeight = image.get_height()
+    pixels = []
+    for h in range(2 * radius + 1):
+        tempPixels = []
+        for w in range(2 * radius + 1):
+            absoluteX = x + (w - radius)
+            absoluteY = y + (h - radius)
+            if radiusSquared >= ((absoluteX - x)**2 + (absoluteY - y)**2) and absoluteX > -1 and absoluteY >-1 and absoluteX < imageWidth and absoluteY < imageHeight:
+                tempPixels.append([absoluteX, absoluteY, "y"])
+            else:
+                tempPixels.append([absoluteX, absoluteY, "n"])
+        pixels.append(tempPixels)
+    return pixels
---
+++ main.py
@@ -0,0 +1,85 @@
+# Import modules
+import pygame, effects, sys
+
+# Define constant values
+maxWidth = 1500
+maxHeight = 900
+
+# Initialise window so pygame will let me load an image
+pygame.display.init()
+pygame.display.set_mode((1,1), 0, 32)
+
+# Read input for image filename
+print("Input the target image's filename")
+valid = False
+while not valid:
+    fileName = input(">")
+    fileType = fileName.split('.')[len(fileName.split('.'))-1]
+    try:
+        if fileType.lower() == "png":
+            image = pygame.image.load(fileName).convert_alpha()
+        else:
+            image = pygame.image.load(fileName)
+        valid = True
+    except:
+        print("File '" + fileName + "' not found!")
+
+# Calculate values to scale image to (Stolen from my pixel sorter)
+scaleFactor = 1
+if image.get_width() > maxWidth:
+    if (maxWidth / image.get_width()) * image.get_height() > maxHeight:
+        scaleFactor = maxHeight / image.get_height()
+    else:
+        scaleFactor = maxWidth / image.get_width()
+
+# Configure pygame
+clock = pygame.time.Clock()
+screen = pygame.surface.Surface((image.get_width() * scaleFactor, image.get_height() * scaleFactor))
+display = pygame.display.set_mode((image.get_width() * scaleFactor, image.get_height() * scaleFactor), 0, 32)
+pygame.display.set_caption("Blurring: " + fileName)
+pygame.display.set_icon(image)
+
+# Read input for blur radius
+radius = -1
+print("Input a blur radius")
+while radius <= 0:
+    radius = input(">")
+    try:
+        radius = int(radius)
+        if radius <= 0:
+            raise Exception
+    except:
+        print("Positive, real integers only!")
+
+# Process image
+image = effects.Blur(image, radius)
+displayImage = pygame.transform.scale(image, (image.get_width() * scaleFactor, image.get_height() * scaleFactor))
+
+# Update window title and image
+pygame.display.set_caption("Blurred: " + fileName)
+pygame.display.set_icon(image)
+
+# Debug function to test fetching circle works
+def DrawDebugCircle(x, y):
+    pixels = effects.FetchPixelsAsCircle(image, x, y, radius)
+    countx, county = 0, 0
+    for column in pixels:
+        county += 1
+        countx = 0
+        for row in column:
+            countx += 1
+            if row[2] == "y":
+                display.set_at((countx, county), (0, 255, 0))  
+# Main loop
+while True:
+    clock.tick(12)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            fileName = fileName.split('.')
+            pygame.image.save(image, "Blurred\\" + fileName[0] + " - Blurred, radius " + str(radius) + "." + fileType)
+            pygame.quit()
+            sys.exit()
+    display.blit(image, (0, 0))
+    #DrawDebugCircle(15, 15)
+    screen.blit(display, (0, 0))
+    pygame.display.update()
